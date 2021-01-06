a8bca5b77abf0e1cc85832cbc478888c4a584a3a
Justin Richer <justin@bspk.io>
2021-01-06 15:46:51 -0500

    retrieve bits

--- deploy.py
+++ deploy.py
@@ -5,8 +5,21 @@ import os
 
 print(os.environ)
 
+artifact_tag = os.environ'[artifact_tag]'
+token = os.environ['GITHUB_TOKEN']
+
 urlbase = 'https://api.github.com/repos/jricher/gnap-core-protocol/actions/artifacts'
 
 with urllib.request.urlopen(urlbase) as url:
     data = json.loads(url.read().decode())
-    print(data)
+
+    for a in data.artifacts:
+        if a.name == os.environ.artifact_tag:
+            print('Downloading artifact %s' % artifact_tag)
+            
+            req = urllib.request.Request(a.archive_download_url, headers={
+                'Authorization: Bearer ' + token
+            })
+            
+            with urllib.request.urlretrieve(req) as local_filename, headers:
+                print('Saved as: %s' % local_filename)
